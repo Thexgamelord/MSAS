0f4852cffe09fbbeb5fdc0611d7b92b53fa9e658
ThexGameLord <69915399+ThexGameLord@users.noreply.github.com>
2022-10-25 09:27:19 +0100

    test

---
+++ index.m
@@ -0,0 +1,72 @@
+<html class="html">
+   
+    
+<style>
+.html {
+    background-image: linear-gradient(45deg, #004affba, #ff0000d6);
+}
+
+#beta_header {
+    margin:center;
+    background:blue;
+}
+</style>
+<head>
+            <title>MSAS | Home</title>
+            <meta name="keywords" content="MSAS website">
+            <meta name="description" content="Website for MSAS">
+<script>
+window.onerror = function (msg, url, line) {
+               alert("Message : " + msg );
+               alert("url : " + url );
+               alert("Line number : " + line );
+            }
+</script>
+
+          
+</head>
+<div id="beta_header">
+<h>MSAS [BETA Site]</h>
+<br>
+<small>MinecraftServerAutoSetup</small></div>
+<br>
+
+<div>
+<button href="LICENSE.md" target="_blank">Go to License</button>
+<br>
+<br>
+<button onclick="dllinux()">Download Latest Release (linux)</button>
+<br>
+<br>
+<button onclick="dlwin()">Download Latest Release (windows)</button>
+<br>
+<br>
+<button onclick="dlwinmenu()">Download Latest menu Release (windows)</button>
+</div>
+
+<body onload="warn()">
+              <script>function warn() {
+                      console.log("Warning: You dont need to be in Here press F12 to exit")
+                      }</script>
+
+
+
+
+
+
+
+
+
+
+                              
+</body>
+
+<style>   
+.cpyright {
+     position: absolute;
+     bottom: 0px;
+}
+</style>
+
+<div class="pt-8 text-center border-t border-gray-700"><center><small class="cpyright">Made by <a href="https://github.com/thexgamelord">Thexgamelord</a></small></center></div>
+</html>
