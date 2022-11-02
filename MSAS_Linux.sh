03559ae4acca0caef6a92af28e3b44054063e171
ThexGameLord <69915399+ThexGameLord@users.noreply.github.com>
2022-11-02 04:01:00 +0000

    fixes msas not loading

--- MSAS_Linux.sh
+++ MSAS_Linux.sh
@@ -8,9 +8,6 @@ echo			MSAS_Linux Menu Coming soon
 echo	   To Check For Updates goto the MSAS Github releases.
 echo		  github.com/ThexGameLord/MSAS/releases
 echo
-sleep 5
 clear
 echo Loading Please Wait as the server has to check the eula.txt
 java -Xms1G -Xmx1G -Dterminal.jline=true -Dterminal.ansi=false -jar server.jar -nogui
-sleep 5
-
