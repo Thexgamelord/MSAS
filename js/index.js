2c59b7229c44ffa0e4bf94017ca9f7dec2aa8987
ThexGameLord <69915399+ThexGameLord@users.noreply.github.com>
2023-01-13 20:24:13 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 function dllinux(){
     alert("check out the releases here for linux builds they currently dont work");
-    window.location("https://github.com/ThexGameLord/MSAS/releases");
+    window.location.href("https://github.com/ThexGameLord/MSAS/releases");
 
 }
 function dlwinmenu(){
     alert("Thank you for downloading MSAS Menu v1.3");
-    window.location("https://github.com/ThexGameLord/MSAS/releases/download/menu_v1.3/MSAS_MENU_v1.3.bat");
+    window.location.href("https://github.com/ThexGameLord/MSAS/releases/download/menu_v1.3/MSAS_MENU_v1.3.bat");
 }
 function dlwin(){
     alert("Thank you for downloading MSAS v1.0");
-    window.location("https://github.com/ThexGameLord/MSAS/releases/download/v1.0/MSAS.exe");
+    window.location.href("https://github.com/ThexGameLord/MSAS/releases/download/v1.0/MSAS.exe");
 }
 function dlold(){
     alert("check out the releases here for old builds");
-    window.location("https://github.com/ThexGameLord/MSAS/releases");
+    window.location.href("https://github.com/ThexGameLord/MSAS/releases");
 
 }
